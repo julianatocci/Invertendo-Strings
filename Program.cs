f1666eb40ff694ba069a7be983156c25bcf8f0d3
Juliana Tocci <142944305+julianatocci@users.noreply.github.com>
2024-03-13 18:39:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InvertendoCaracteres
+{
+    class Program
+    {
+            static void Main()
+            {
+                Console.WriteLine("Digite uma frase: ");
+                string frase = Console.ReadLine();
+                string resultado = InverterString(frase);
+                Console.WriteLine("String invertida: " + resultado);
+                Console.ReadLine();
+            }
+            static string InverterString(string inputString)
+            {
+            char[] caracteres = inputString.ToCharArray();
+            int inicial = 0;
+            int final = caracteres.Length - 1;
+
+            while (inicial < final)
+            {
+                (caracteres[inicial], caracteres[final]) = (caracteres[final], caracteres[inicial]);
+                inicial++;
+                final--;
+            }
+            string resultado = new string(caracteres);
+            return resultado;
+            }
+    }
+ }
